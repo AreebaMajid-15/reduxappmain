984dd1053032fb9a5a6ea60ece3bd205f3aca275
AreebaMajid-15 <areebamajid04@gmail.com>
2025-03-01 00:20:30 +0530

    basic structure added

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,19 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCR3mKjxg9IuIr8rHHGHkNBnoGd9DO_s84",
+  authDomain: "reactappmain-320ef.firebaseapp.com",
+  projectId: "reactappmain-320ef",
+  storageBucket: "reactappmain-320ef.firebasestorage.app",
+  messagingSenderId: "81268384887",
+  appId: "1:81268384887:web:9255f0a935ce7896f7d272"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default  app ;
